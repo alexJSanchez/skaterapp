26bd66c710da917b9ae740aee6917cf5c9dbcb0a
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2024-02-24 06:06:35 -0500

    midtown data base

---
+++ src/assets/midTown/midTownData.js
@@ -0,0 +1,195 @@
+import BellevuePark0 from "./BellevuePark/0.jpeg";
+import BellevuePark1 from "./BellevuePark/1.jpeg";
+import BellevuePark2 from "./BellevuePark/2.jpeg";
+import BellevuePark3 from "./BellevuePark/3.jpeg";
+import BellevuePark4 from "./BellevuePark/4.jpeg";
+import BellevuePark5 from "./BellevuePark/5.jpeg";
+import BigScreenLedges0 from "./BigScreenLedges/0.jpg";
+import BigScreenLedges1 from "./BigScreenLedges/1.jpg";
+import BigScreenLedges2 from "./BigScreenLedges/2.jpg";
+import BigScreenLedges3 from "./BigScreenLedges/3.jpg";
+import BigScreenLedges4 from "./BigScreenLedges/4.jpg";
+import BigScreenLedges5 from "./BigScreenLedges/5.jpg";
+import Brick0 from "./Brick9/0.jpg";
+import Brick1 from "./Brick9/1.jpg";
+import Brick2 from "./Brick9/2.jpg";
+import Brick3 from "./Brick9/3.jpg";
+import Brick4 from "./Brick9/4.jpg";
+import Brick5 from "./Brick9/5.jpg";
+import Brick6 from "./Brick9/6.jpg";
+import BryantPark0 from "./BryantPark/0.jpg";
+import BryantPark1 from "./BryantPark/1.jpg";
+import BryantPark2 from "./BryantPark/2.jpg";
+import BryantPark3 from "./BryantPark/3.jpg";
+import BryantPark4 from "./BryantPark/4.jpg";
+import BryantPark5 from "./BryantPark/5.jpg";
+import BryantPark6 from "./BryantPark/6.jpg";
+import BryantPark7 from "./BryantPark/7.jpg";
+import BryantPark8 from "./BryantPark/8.jpg";
+import CarWashBump0 from "./BryantPark/0.jpg";
+import CarWashBump1 from "./BryantPark/1.jpg";
+import CBS0 from "./cbs/0.jpeg";
+import CBS1 from "./cbs/1.jpeg";
+import CBS2 from "./cbs/2.jpeg";
+import CBS3 from "./cbs/3.jpeg";
+import CBS4 from "./cbs/4.jpeg";
+import CBS5 from "./cbs/5.jpeg";
+import CBS6 from "./cbs/6.jpeg";
+import CBS7 from "./cbs/7.jpeg";
+import CBS8 from "./cbs/.jpeg";
+
+const spots = [
+  {
+    name: "Bellevue Park",
+    area: "Midtown",
+    summary:
+      "Three consecutive sets of five stairs with kinked rails down them, low handicap ramp rails, and the occasional pile of human excrement.",
+    bust: {
+      level: 2,
+      status:
+        "Skateboarding is forbidden by park rules. The Parks Department usually does not hassle you though, but it has been known to happen on rare occasions.",
+    },
+
+    location: {
+      latitude: "40.74088747977921",
+      longitude: "-73.9790578132715",
+      text: "27th Street and Second Avenue in Kips Bay.",
+    },
+    images: {
+      0: BellevuePark0,
+      1: BellevuePark1,
+      3: BellevuePark2,
+      4: BellevuePark3,
+      5: BellevuePark4,
+      6: BellevuePark5,
+    },
+  },
+  {
+    name: "Big Screen Ledges",
+    area: "Midtown",
+    summary:
+      "Marble ledges everywhere on amazing ground, two of which go out past a three stair. One of the best spots in New York, by far. Spot is now knobbed",
+    bust: {
+      level: 4,
+      status:
+        "Skateboarding is forbidden in the plaza. Security will come out and tell you to leave as soon as they spot you on the cameras.",
+    },
+
+    location: {
+      latitude: "40.747341960924686",
+      longitude: "-73.98961368221164",
+      text: "30th Street between 6th and 7th Avenues, though much closer to 6th.",
+    },
+    images: {
+      0: BigScreenLedges0,
+      1: BigScreenLedges1,
+      3: BigScreenLedges2,
+      4: BigScreenLedges3,
+      5: BigScreenLedges4,
+      6: BigScreenLedges5,
+    },
+  },
+  {
+    name: "Brick 9",
+    area: "Midtown",
+    summary:
+      "Two sets of nine stairs, the second of which has thin square handrail. Also an eight stair with a 45-degree handrail going down the middle of it.",
+    bust: {
+      level: 4,
+      status:
+        "There are “No Skateboarding” signs posted. The building is a huge bust because it is near a lot of embassies. Security will come out in a matter of minutes and make you leave — sometimes the cops will too. The spot is also fenced off at night.",
+    },
+
+    location: {
+      latitude: "40.75310448877658",
+      longitude: "-73.96988521267978",
+      text: "47th Street, in between 2nd Avenue and 3rd Avenue.",
+    },
+    images: {
+      0: Brick0,
+      1: Brick1,
+      2: Brick2,
+      3: Brick3,
+      4: Brick4,
+      5: Brick5,
+      6: Brick6,
+    },
+  },
+  {
+    name: "Bryant Park",
+    area: "Midtown",
+    summary:
+      "Some steps, a beveled up-ledge, and a round frontside-for-regular rail down seven stairs.",
+    bust: {
+      level: 3,
+      status:
+        "Skateboarding is forbidden by park rules. Parks security will tell you to leave as soon as they spot you. Better at night, but kinda dark.",
+    },
+
+    location: {
+      latitude: "40.753856858769325",
+      longitude: "-73.98094264448864",
+      text: "42nd Street, in between 5th Avenue and 6th Avenue.",
+    },
+    images: {
+      0: BryantPark0,
+      1: BryantPark1,
+      2: BryantPark2,
+      3: BryantPark3,
+      4: BryantPark4,
+      5: BryantPark5,
+      6: BryantPark6,
+      6: BryantPark7,
+      6: BryantPark8,
+    },
+  },
+  {
+    name: "Car Wash Bump",
+    area: "Midtown",
+    summary:
+      "Probably the second most famous bump-to-bar spot in New York, after the one on Avenue D.",
+    bust: {
+      level: 1,
+      status:
+        "The car wash closed down, so the building is empty for now. Just ensure somebody is spotting for your board so it does not shoot out into the road.",
+    },
+
+    location: {
+      latitude: "40.76493313377015",
+      longitude: "-73.99804331778756",
+      text: "47th Street and the Westside Highway.",
+    },
+    images: {
+      0: CarWashBump0,
+      1: CarWashBump1,
+    },
+  },
+  {
+    name: "CBS",
+    area: "Midtown",
+    summary:
+      "A close colleague described this spot as “New York good” — it looks like Europe in footage, but then you show up, and it’s definitely New York.The ledges are rougher than they seem, the ground has some rugged patches, and the stone never stays broken in, e.g. they need to be re-waxed every time you skate here. That being said, it is a block-wide marble plaza full of ledges, and one of the funnest skate spots in New York.",
+    bust: {
+      level: 4,
+      status: "",
+    },
+
+    location: {
+      latitude: "40.761456551336046",
+      longitude: "-73.97954718664414",
+      text: "There are “No Skateboarding” signs posted. Security from the building will often tell you to leave in ten minutes or less. Also gets crowded because the MoMA is across the street.",
+    },
+    images: {
+      0: CBS0,
+      1: CBS1,
+      2: CBS2,
+      3: CBS3,
+      4: CBS4,
+      5: CBS5,
+      6: CBS6,
+      7: CBS7,
+      8: CBS8,
+    },
+  },
+];
+export default spots;
