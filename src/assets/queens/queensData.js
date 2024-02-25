72a254c225904d595632e7d5d0667a69247908bd
alex sanchez <alexjsanchez1020@gmail.com>
2024-02-25 13:03:09 -0500

    data base done

---
+++ src/assets/queens/queensData.js
@@ -0,0 +1,463 @@
+import AthensPark0 from "./AthensPark/0.jpg";
+import AthensPark1 from "./AthensPark/1.jpg";
+import AthensPark2 from "./AthensPark/2.jpg";
+import AthensPark3 from "./AthensPark/3.jpg";
+import AthensPark4 from "./AthensPark/4.jpg";
+import BroadwayPark0 from "./BroadwayPark/0.jpg";
+import BroadwayPark1 from "./BroadwayPark/1.jpg";
+import BroadwayPark2 from "./BroadwayPark/2.jpg";
+import BroadwayPark3 from "./BroadwayPark/3.jpg";
+import BroadwayPark4 from "./BroadwayPark/4.jpg";
+import CemeteryCurbs0 from "./CemeteryCurbs/0.jpg";
+import CemeteryCurbs1 from "./CemeteryCurbs/1.jpg";
+import CemeteryCurbs2 from "./CemeteryCurbs/2.jpg";
+import CemeteryCurbs3 from "./CemeteryCurbs/3.jpg";
+import CemeteryCurbs4 from "./CemeteryCurbs/4.jpg";
+import CemeteryCurbs5 from "./CemeteryCurbs/5.jpg";
+import CemeteryCurbs6 from "./CemeteryCurbs/6.jpg";
+import CemeteryCurbs7 from "./CemeteryCurbs/7.jpg";
+import CemeteryCurbs8 from "./CemeteryCurbs/8.jpg";
+import CitiFieldBenches0 from "./CitiFieldBenches/0.jpg";
+import CitiFieldBenches1 from "./CitiFieldBenches/1.jpg";
+import CitiFieldBenches2 from "./CitiFieldBenches/2.jpg";
+import CitiFieldBenches3 from "./CitiFieldBenches/3.jpg";
+import ConEdisonBanks0 from "./ConEdisonBanks/0.jpg";
+import ConEdisonBanks1 from "./ConEdisonBanks/1.jpg";
+import ConEdisonBanks2 from "./ConEdisonBanks/2.jpg";
+import ConEdisonBanks3 from "./ConEdisonBanks/3.jpg";
+import ConEdisonBanks4 from "./ConEdisonBanks/4.jpg";
+import ConEdisonBanks5 from "./ConEdisonBanks/5.jpg";
+import FlushingMeadowsPark0 from "./FlushingMeadowsPark/0.jpg";
+import FlushingMeadowsPark1 from "./FlushingMeadowsPark/1.jpg";
+import FlushingMeadowsPark2 from "./FlushingMeadowsPark/2.jpg";
+import FlushingMeadowsPark3 from "./FlushingMeadowsPark/3.jpg";
+import FlushingMeadowsPark4 from "./FlushingMeadowsPark/4.jpg";
+import FlushingMeadowsPark5 from "./FlushingMeadowsPark/5.jpg";
+import FlushingMeadowsPark6 from "./FlushingMeadowsPark/6.jpg";
+import FlushingMeadowsPark7 from "./FlushingMeadowsPark/7.jpg";
+import FlushingMeadowsPark8 from "./FlushingMeadowsPark/8.jpg";
+import FlushingMeadowsPark9 from "./FlushingMeadowsPark/9.jpg";
+import FlushingMeadowsPark10 from "./FlushingMeadowsPark/10.jpg";
+import FlushingMeadowsPark11 from "./FlushingMeadowsPark/11.jpg";
+import HillcrestHighSchool0 from "./HillcrestHighSchool/0.jpg";
+import HillcrestHighSchool1 from "./HillcrestHighSchool/1.jpg";
+import HillcrestHighSchool2 from "./HillcrestHighSchool/2.jpg";
+import HillcrestHighSchool3 from "./HillcrestHighSchool/3.jpg";
+import HillcrestHighSchool4 from "./HillcrestHighSchool/4.jpg";
+import LongIslandCityWaterfront0 from "./LongIslandCityWaterfront/0.jpg";
+import LongIslandCityWaterfront1 from "./LongIslandCityWaterfront/1.jpg";
+import LongIslandCityWaterfront2 from "./LongIslandCityWaterfront/2.jpg";
+import LongIslandCityWaterfront3 from "./LongIslandCityWaterfront/3.jpg";
+import LongIslandCityWaterfront4 from "./LongIslandCityWaterfront/4.jpg";
+import LongIslandCityWaterfront5 from "./LongIslandCityWaterfront/5.jpg";
+import LongIslandCityWaterfront6 from "./LongIslandCityWaterfront/6.jpg";
+import LongIslandCityWaterfront7 from "./LongIslandCityWaterfront/7.jpg";
+import LongIslandCityWaterfront8 from "./LongIslandCityWaterfront/8.jpg";
+import LongIslandCityWaterfront9 from "./LongIslandCityWaterfront/9.jpg";
+import LongIslandCityWaterfront10 from "./LongIslandCityWaterfront/10.jpg";
+import LongIslandCityWaterfront11 from "./LongIslandCityWaterfront/11.jpg";
+import LongIslandCityWaterfront12 from "./LongIslandCityWaterfront/12.jpg";
+import LongIslandCityWaterfront13 from "./LongIslandCityWaterfront/13.jpg";
+import JFKBanks0 from "./JFKBanks/0.jpg";
+import JFKBanks1 from "./JFKBanks/1.jpg";
+import JFKBanks2 from "./JFKBanks/2.jpg";
+import JFKBanks3 from "./JFKBanks/3.jpg";
+import JFKBanks4 from "./JFKBanks/4.jpg";
+import JFKBanks5 from "./JFKBanks/5.jpg";
+import JFKBanks6 from "./JFKBanks/6.jpg";
+import JFKBanks7 from "./JFKBanks/7.jpg";
+import MosquitoBeach0 from "./MosquitoBeach/0.jpg";
+import MosquitoBeach1 from "./MosquitoBeach/1.jpg";
+import MosquitoBeach2 from "./MosquitoBeach/2.jpg";
+import MosquitoBeach3 from "./MosquitoBeach/3.jpg";
+import MosquitoBeach4 from "./MosquitoBeach/4.jpg";
+import MosquitoBeach5 from "./MosquitoBeach/5.jpg";
+import MosquitoBeach6 from "./MosquitoBeach/6.jpg";
+import PS2390 from "./PS239/0.jpg";
+import PS2391 from "./PS239/1.jpg";
+import PS2392 from "./PS239/2.jpg";
+import PS2393 from "./PS239/3.jpg";
+import RockawayRails0 from "/RockawayRails/0.jpg";
+import RockawayRails1 from "/RockawayRails/1.jpg";
+import RockawayRails2 from "/RockawayRails/2.jpg";
+import RockawayRails3 from "/RockawayRails/3.jpg";
+import RooseveltIslandLedge0 from "/RooseveltIslandLedge/0.jpg";
+import RooseveltIslandLedge1 from "/RooseveltIslandLedge/1.jpg";
+import RooseveltIslandLedge2 from "/RooseveltIslandLedge/2.jpg";
+import TheSombrero0 from "/TheSombrero/0.jpg";
+import TheSombrero1 from "/TheSombrero/1.jpg";
+import TheSombrero2 from "/TheSombrero/2.jpg";
+import TheSombrero3 from "/TheSombrero/3.jpg";
+import TheSombrero4 from "/TheSombrero/4.jpg";
+import VernonJackson0 from "/VernonJackson/0.jpg";
+import VernonJackson1 from "/VernonJackson/1.jpg";
+import VernonJackson2 from "/VernonJackson/2.jpg";
+import VernonJackson3 from "/VernonJackson/3.jpg";
+import VernonJackson4 from "/VernonJackson/4.jpg";
+import VernonJackson5 from "/VernonJackson/5.jpg";
+
+const spots = [
+	{
+		name: "Athens Park",
+		area: "Queens",
+		summary: "A couple of three-stair sets with low ledges running over them.",
+		bust: {
+			level: 1,
+			status:
+				"Nobody typically bothers you here, just avoid during peak park hours because it’ll be overrun with kids.",
+		},
+		location: {
+			latitude: 40.76813540148237,
+			longitude: -73.92231054222408,
+			text: "30th Street and 30th Avenue in Astoria, Queens.",
+		},
+		images: {
+			0: AthensPark0,
+			1: AthensPark1,
+			2: AthensPark2,
+			3: AthensPark3,
+			4: AthensPark4,
+		},
+	},
+	{
+		name: "Broadway Park",
+		area: "Queens",
+		summary:
+			"A three-flat-three with ample runway + a three-block and some low ledges.",
+		bust: {
+			level: 5,
+			status:
+				"Nobody typically bothers you here, just avoid during peak park hours because it’ll be overrun with kids.",
+		},
+		location: {
+			latitude: 40.742347639296455,
+			longitude: -73.882647271139,
+			text: "Broadway and 45th Avenue in Elmhurst, Queens.",
+		},
+		images: {
+			0: BroadwayPark0,
+			1: BroadwayPark1,
+			2: BroadwayPark2,
+			3: BroadwayPark3,
+			4: BroadwayPark4,
+		},
+	},
+	{
+		name: "Cemetery Curbs",
+		area: "Queens",
+		summary:
+			"A bunch of curbs, a random asphalt hip, and a small bank-to-curb on a mellow hill.",
+		bust: {
+			level: 1,
+			status:
+				"You don’t get booted, but everything is approached from an active roadway, so be mindful of cars.",
+		},
+		location: {
+			latitude: 40.73225737036463,
+			longitude: -73.92602154220934,
+			text: "Laurel Hill Boulevard and 54th Road, essentially right over the Queens side of the Kosciuszko Bridge.",
+		},
+		images: {
+			0: CemeteryCurbs0,
+			1: CemeteryCurbs1,
+			2: CemeteryCurbs2,
+			3: CemeteryCurbs3,
+			4: CemeteryCurbs4,
+			5: CemeteryCurbs5,
+			6: CemeteryCurbs6,
+			7: CemeteryCurbs7,
+			8: CemeteryCurbs8,
+		},
+	},
+	{
+		name: "Citi Field Benches",
+		area: "Queens",
+		summary:
+			"A couple of benches on decent ground. Usually a back-up plan for when the Globe is a bust or the fountains are on.",
+		bust: {
+			level: 1,
+			status:
+				"This park is always empty unless there is a Mets game going on. Nobody really bothers you.",
+		},
+		location: {
+			latitude: 40.7619993947189,
+			longitude: -73.84657513041199,
+			text: "There is no cross street here, but they’re behind Citi Field on the Flushing Bay Promenade. The benches will be visible under two small white amphitheater type structures.",
+		},
+		images: {
+			0: CitiFieldBenches0,
+			1: CitiFieldBenches1,
+			2: CitiFieldBenches2,
+			3: CitiFieldBenches3,
+		},
+	},
+	{
+		name: "Con-Edison Banks",
+		area: "Queens",
+		summary:
+			"Rough brick banks on even rougher ground, and broken glass everywhere. Also, a ledge between two of those banks. Having big wheels would definitely help your cause. Bring wax.",
+		bust: {
+			level: 1,
+			status: "Nobody bothers you here.",
+		},
+		location: {
+			latitude: 40.779970345626296,
+			longitude: -73.90627145776968,
+			text: "20th Avenue and 31st Street in Astoria.",
+		},
+		images: {
+			0: ConEdisonBanks0,
+			1: ConEdisonBanks1,
+			2: ConEdisonBanks2,
+			3: ConEdisonBanks3,
+			4: ConEdisonBanks4,
+			5: ConEdisonBanks5,
+		},
+	},
+	{
+		name: "Flushing Meadows Park",
+		area: "Queens",
+		summary:
+			"One of the most famous skate spots in New York. Ledges and a grate gap. Manual pad down a long six stair.",
+		bust: {
+			level: 3,
+			status:
+				"You’re not supposed to be inside the fountain. The bust can be random. On occasion, a Parks Department patrol will tell you to leave the inside of the fountain and instruct you to go to the nearby skatepark (which sucks.) If you have horrible luck, they may issue you a summons.",
+		},
+		location: {
+			latitude: 40.746771026920456,
+			longitude: -73.84506799999153,
+			text: "Corona Park / Flushing Meadows Park, under the Unisphere sculpture.",
+		},
+		images: {
+			0: FlushingMeadowsPark0,
+			1: FlushingMeadowsPark1,
+			2: FlushingMeadowsPark2,
+			3: FlushingMeadowsPark3,
+			4: FlushingMeadowsPark4,
+			5: FlushingMeadowsPark5,
+			6: FlushingMeadowsPark6,
+			7: FlushingMeadowsPark7,
+			8: FlushingMeadowsPark8,
+			9: FlushingMeadowsPark9,
+			10: FlushingMeadowsPark10,
+			11: FlushingMeadowsPark11,
+		},
+	},
+	{
+		name: "Hillcrest High School",
+		area: "Queens",
+		summary: "Brick banks and a low ledge over a six stair.",
+		bust: {
+			level: 2,
+			status: "You are not allowed to skate on school property. Weekend spot.",
+		},
+		location: {
+			latitude: 40.71018570600576,
+			longitude: -73.80164091556222,
+			text: "Parsons Boulevard and Hillside Avenue in Jamaica, Queens.",
+		},
+		images: {
+			0: HillcrestHighSchool0,
+			1: HillcrestHighSchool1,
+			2: HillcrestHighSchool2,
+			3: HillcrestHighSchool3,
+			4: HillcrestHighSchool4,
+		},
+	},
+	{
+		name: "Long Island City Waterfront",
+		area: "Queens",
+		summary:
+			"Wooden benches that can be skated as banks you ollie up to, weird cement ledges, long triangular flat rails, big concrete wallrides on a wooden platform — a lot of different stuff with varying bust levels.",
+		bust: {
+			level: 5,
+			status:
+				"There is no skateboarding allowed in the park, and the Parks Department patrols it heavily. They’ll tell you to leave as soon as they see you, or possibly issue you a summons if you’re unlucky.",
+		},
+		location: {
+			latitude: 40.745863005055384,
+			longitude: -73.95722945778292,
+			text: "47th Road and Center Boulevard in Long Island City, Queens.",
+		},
+		images: {
+			0: LongIslandCityWaterfront0,
+			1: LongIslandCityWaterfront1,
+			2: LongIslandCityWaterfront2,
+			3: LongIslandCityWaterfront3,
+			4: LongIslandCityWaterfront4,
+			5: LongIslandCityWaterfront5,
+			6: LongIslandCityWaterfront6,
+			7: LongIslandCityWaterfront7,
+			8: LongIslandCityWaterfront8,
+			9: LongIslandCityWaterfront9,
+			10: LongIslandCityWaterfront10,
+			11: LongIslandCityWaterfront11,
+			12: LongIslandCityWaterfront12,
+			13: LongIslandCityWaterfront13,
+		},
+	},
+	{
+		name: "JFK Banks",
+		area: "Queens",
+		summary:
+			"Cement banks with a ledge on top and hips at the end. Seen in videos as far back as the 1980s.",
+		bust: {
+			level: 4,
+			status:
+				"It’s at an airport, obviously you are not allowed to skate here. Police will make the rounds and tell you to leave if they spot you. People get time though.",
+		},
+		location: {
+			latitude: 40.648565974607635,
+			longitude: -73.77950480449134,
+			text: "Terminal 6 at John F. Kennedy International Airport in Jamaica, Queens. It’s not worth taking a train out here, because it involves transfers to the Airtrain. Best to drive to, or worth a shot if you’re picking up a friend from the airport :)",
+		},
+		images: {
+			0: JFKBanks0,
+			1: JFKBanks1,
+			2: JFKBanks2,
+			3: JFKBanks3,
+			4: JFKBanks4,
+			5: JFKBanks5,
+			6: JFKBanks6,
+			7: JFKBanks7,
+		},
+	},
+	{
+		name: "Mosquito Beach",
+		area: "Queens",
+		summary:
+			"Constantly evolving concrete D.I.Y. spot with banks, transitions and ledges.",
+		bust: {
+			level: 1,
+			status: "Street is blocked off. Nobody really bothers you.",
+		},
+		location: {
+			latitude: 40.73952507356028,
+			longitude: -73.94078308425311,
+			text: "Borden Avenue and Review Avenue in Long Island City, adjacent to the FedEx parking lot. Easiest way to find it is searching Sparks Deli.",
+		},
+		images: {
+			0: MosquitoBeach0,
+			1: MosquitoBeach1,
+			2: MosquitoBeach2,
+			3: MosquitoBeach3,
+			4: MosquitoBeach4,
+			5: MosquitoBeach5,
+			6: MosquitoBeach6,
+		},
+	},
+	{
+		name: "P.S. 239",
+		area: "Queens",
+		summary:
+			"A three-stair Philly step and a curved ledge / platform on basketball court ground.",
+		bust: {
+			level: 1,
+			status:
+				"You are technically not allowed to skate on school property, but as long as school is not in session, nobody really hassles you.",
+		},
+		location: {
+			latitude: 40.699465851688814,
+			longitude: -73.90383643112348,
+			text: "Weirfield Street and Seneca Avenue in Ridgewood, Queens. (Note: some people will still refer to this zone as Brooklyn since it is right at the border.)",
+		},
+		images: {
+			0: PS2390,
+			1: PS2391,
+			2: PS2392,
+			3: PS2393,
+		},
+	},
+	{
+		name: "Rockaway Rails",
+		spots: "Queens",
+		summary:
+			"The best handrail spot in New York. Fat square rails down seven big steps.",
+		bust: {
+			level: 3,
+			status:
+				"There are “No Skateboarding” signs posted. Weekend spot, but there still may be police officers stationed inside the school on weekends. They will tell you to leave if they see you. If you get kicked out in the summer, at least it’s right at the beach ;)",
+		},
+		location: {
+			latitude: 40.58623976470392,
+			longitude: -73.82322700000779,
+			text: "At Beach Channel High School, located on Beach Channel Drive and Beach 101st Street in Far Rockaway, Queens. It’s best to drive here. The train takes an hour-and-a-half from Manhattan.",
+		},
+		images: {
+			0: RockawayRails0,
+			1: RockawayRails1,
+			2: RockawayRails2,
+			3: RockawayRails3,
+		},
+	},
+	{
+		name: "Roosevelt Island Ledge",
+		spots: "Queens",
+		summary:
+			"Photogenic spot that you’ve seen a million photos at. Backside for regular marble ledge over a long three stair. Wooden landing.",
+		bust: {
+			level: 3,
+			status:
+				"You’re not allowed to skateboard here. Security from the adjacent building may tell you to leave. It’s better during cooler months, because during the summer, there is often an outdoor cafe set up in the runway for the spot.",
+		},
+		location: {
+			latitude: 40.75994361707782,
+			longitude: -73.95321099999897,
+			text: "Right outside the F train stop on Roosevelt Island. Take the F to Roosevelt Island, walk out onto W Road and towards the water. Make a right and you should see the spot.",
+		},
+		images: {
+			0: RooseveltIslandLedge0,
+			1: RooseveltIslandLedge1,
+			2: RooseveltIslandLedge2,
+		},
+	},
+	{
+		name: "The Sombrero",
+		area: "Queens",
+		summary:
+			"Big sombrero-shaped piece of concrete that people wallie up to and do manuals / ledge tricks on + a couple chunky concrete ledges.",
+		bust: {
+			level: 1,
+			status:
+				"It’s on the side of the highway and nobody else ever hangs out here. You should be good.",
+		},
+		location: {
+			latitude: 40.72616640685465,
+			longitude: -73.9042499156007,
+			text: "Borden Avenue and 61st Street (parallel to the L.I.E.) in Maspeth, Queens. Most people drive to here, since there are no trains nearby.",
+		},
+		images: {
+			0: TheSombrero0,
+			1: TheSombrero1,
+			2: TheSombrero2,
+			3: TheSombrero3,
+			4: TheSombrero4,
+		},
+	},
+	{
+		name: "Vernon-Jackson",
+		area: "Queens",
+		summary:
+			"High frontside for regular out-ledge over a four, some metal benches, and a metal bench (frontside for regular also) over a ledge gap.",
+		bust: {
+			level: 1,
+			status:
+				"Typically a bust-free spot, but there will be basketball games going on during the summer months.",
+		},
+		location: {
+			latitude: 40.74440049783073,
+			longitude: -73.95367191552515,
+			text: "Vernon Boulevard and 48th Avenue in Long Island City, Queens.",
+		},
+		images: {
+			0: VernonJackson0,
+			1: VernonJackson1,
+			2: VernonJackson2,
+			3: VernonJackson3,
+			4: VernonJackson4,
+			5: VernonJackson5,
+		},
+	},
+];
+export default spots;
---
+++ src/assets/statenIsland/statenIslandData.js
@@ -0,0 +1,31 @@
+import ABCLedges0 from "/ABCLedges/0.jpg";
+import ABCLedges1 from "/ABCLedges/1.jpg";
+import ABCLedges2 from "/ABCLedges/2.jpg";
+import ABCLedges3 from "/ABCLedges/3.jpg";
+import ABCLedges4 from "/ABCLedges/4.jpg";
+
+const spots = [
+	{
+		name: "ABC Ledges",
+		area: "Staten Island",
+		summary:
+			"Four double-sided concrete ledges in a schoolyard on parking lot-esque ground. They’ve been skated for two decades at this point, so they’ve seen better days.",
+		bust: {
+			level: 1,
+			status: "You’re not allowed to skate here. Weekend spot.",
+		},
+		location: {
+			latitude: 40.510720065535835,
+			longitude: -74.23051845779361,
+			text: "Page Avenue and Haywood Street in Staten Island. It’s only accessible by car and very far from Manhattan.",
+		},
+		images: {
+			0: ABCLedges0,
+			1: ABCLedges1,
+			2: ABCLedges2,
+			3: ABCLedges3,
+			4: ABCLedges4,
+		},
+	},
+];
+export default spots;
