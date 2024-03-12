0bd862cb965482d1bfce9e5e0e5098cf6da6002a
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-12 06:16:15 -0400

    add vue proptypes

--- src/component/popup.jsx
+++ src/component/popup.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import X from "../assets/X.png";
+import Xicon from "../assets/X.png";
 import ImageSlider from "./slider.jsx";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function Popup({ Name, Bust, Summary, Status, Images, Star, HandlePopup }) {
 	return (
 		<div className="fixed inset-0 flex items-center justify-center text-center bg-black bg-opacity-50 z-50">
 			<div className="bg-white p-4 rounded-lg shadow-lg m-4">
 				<div onClick={HandlePopup} className="flex justify-end">
-					<img className="w-4 text-right" src={X} />
+					<img className="w-4 text-right" src={Xicon} />
 				</div>
 				<h1 className="text-2xl font-bold text-gray-800 uppercase">{Name}</h1>
 				<div className="flex justify-center">
@@ -25,7 +25,6 @@ function Popup({ Name, Bust, Summary, Status, Images, Star, HandlePopup }) {
 				<p className="text-gray-600 font-bold text-sm">Status: {Status}</p>
 
 				<p className="text-gray-700 font-bold">{Summary}</p>
-				<Link to=""></Link>
 				<div
 					style={{
 						borderStyle: "dotted",
@@ -38,5 +37,14 @@ function Popup({ Name, Bust, Summary, Status, Images, Star, HandlePopup }) {
 		</div>
 	);
 }
+Popup.propTypes = {
+	Name: PropTypes.string.isRequired,
+	Bust: PropTypes.number.isRequired,
+	Summary: PropTypes.string.isRequired,
+	Status: PropTypes.string.isRequired,
+	Images: PropTypes.arrayOf(PropTypes.string).isRequired,
+	Star: PropTypes.string.isRequired,
+	HandlePopup: PropTypes.func.isRequired,
+};
 
 export default Popup;
