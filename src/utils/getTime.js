9b68781106f6194243f4bad748b16335c6215dd1
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-13 02:52:15 -0400

    creating a function for time

---
+++ src/utils/getTime.js
@@ -0,0 +1,20 @@
+const GetTime = () => {
+	let currentTime = new Date();
+	let timeOfDay = "";
+	const hours = currentTime.getHours();
+	const minutes = currentTime.getMinutes();
+	const seconds = currentTime.getSeconds();
+	if (hours >= 0 && hours < 6) {
+		timeOfDay = "Good Morning";
+	} else if (hours >= 6 && hours < 12) {
+		timeOfDay = "Good Morning";
+	} else if (hours >= 12 && hours < 18) {
+		timeOfDay = "Good Afternoon";
+	} else {
+		timeOfDay = "Good Evening";
+	}
+	const convertedHours = hours % 12 || 12;
+	const isDaytime = hours >= 6 && hours < 18;
+	return { minutes, seconds, convertedHours, isDaytime, timeOfDay };
+};
+export default GetTime;
