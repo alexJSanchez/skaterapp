f92e6468950c908adab929cd9887f607d67f1d1b
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-07 02:03:55 -0400

    refactor coords

--- src/Coord.js
+++ src/Coord.js
@@ -2,70 +2,24 @@ import Data from "./Data";
 
 const locations = [];
 
-Data.Bronx.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
+const mapLocations = (locationData) => {
+	locationData.forEach((spot) => {
+		locations.push({
+			name: spot.name,
+			longitude: spot.location.longitude,
+			latitude: spot.location.latitude,
+		});
 	});
-});
-Data.Brooklyn.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
-Data.FinancialDistrict.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
+};
 
-Data.LowerEastSide.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
-Data.Midtown.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
-Data.Queens.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
-Data.StatenIsland.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
+mapLocations(Data.Bronx);
+mapLocations(Data.Brooklyn);
+mapLocations(Data.FinancialDistrict);
+mapLocations(Data.LowerEastSide);
+mapLocations(Data.Midtown);
+mapLocations(Data.Queens);
+mapLocations(Data.StatenIsland);
+mapLocations(Data.UptownHarlem);
+mapLocations(Data.WestVillageTribeca);
 
-Data.UptownHarlem.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
-
-Data.WestVillageTribeca.map((spot) => {
-	locations.push({
-		name: spot.name,
-		longitude: spot.location.longitude,
-		latitude: spot.location.latitude,
-	});
-});
 export default locations;
