19adb614c6e25828f907ee68493be932d02fc654
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-07 02:13:19 -0400

    refactor randomskatetricks

--- src/utils/randomSkateTricks.js
+++ src/utils/randomSkateTricks.js
@@ -1,5 +1,5 @@
 const trickList = {
-	stance: ["switch", "nollie", "fackie", " "],
+	stance: ["switch", "nollie", "fackie"],
 	flips: [
 		"kickflip",
 		"heelflip",
@@ -7,16 +7,15 @@ const trickList = {
 		"varialheel",
 		"treflip",
 		"popshovit",
-		" ",
 	],
 	spins: ["", "180", "360"],
 };
 
-const getTricks = (trickList) => {
+const generateRandomTrick = (trickList) => {
 	const randomFlip = Math.floor(Math.random() * trickList.flips.length);
 	const randomSpin = Math.floor(Math.random() * trickList.spins.length);
 	const randomStance = Math.floor(Math.random() * trickList.stance.length);
 	return `${trickList.stance[randomStance]} ${trickList.spins[randomSpin]} ${trickList.flips[randomFlip]}`;
 };
 
-export { trickList, getTricks };
+export { trickList, generateRandomTrick };
