80a3471a4f98729ceef0a0d788989ba1b431c8d9
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-09 03:58:43 -0400

    add margin

--- src/layout/westVillageTribeca.jsx
+++ src/layout/westVillageTribeca.jsx
@@ -18,7 +18,7 @@ function WestVillageTribeca() {
 				</Link>
 			</div>
 			<div
-				className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 text-center p-4 "
+				className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 text-center p-4 mt-10"
 				style={{ backgroundImage: `url(${gripTape})` }}
 			>
 				{spots.WestVillageTribeca.map((res, index) => (
