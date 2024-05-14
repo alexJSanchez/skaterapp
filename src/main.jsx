7e738a93bc2b7077f13d206273e186f055a7b213
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-14 03:00:33 -0400

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Auth0ProviderWithHistory>
+				<App />
+			</Auth0ProviderWithHistory>
+		</BrowserRouter>
 	</React.StrictMode>
 );
