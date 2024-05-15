ab12aeb6b163c623d2b606a1040bb69c9254cf2c
alex sanchez <alexjsanchez1020@gmail.com>
2024-05-15 00:25:53 -0400

    move authprovider to mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Auth0Provider
+			domain={import.meta.env.REACT_APP_AUTH0_DOMAIN}
+			clientId={import.meta.env.REACT_APP_AUTH0_CLIENT_ID}
+			authorizationParams={{
+				redirect_uri: window.location.origin,
+			}}
+		>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</Auth0Provider>
+	</BrowserRouter>
 );
